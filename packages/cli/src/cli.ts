e85364a9753d2d5fb9cc62727c5d8ef09f111fa3
Prathamesh Chougale <prathamesh17170@gmail.com>
2025-11-08 16:55:04 +0530

    feat: update CLI version retrieval to read from package.json

--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -1,5 +1,14 @@
 import { Command } from "commander";
+import { readFileSync } from "fs";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 import { createDetectCommand, createMigrateCommand } from "./commands/index.js";
+
+// Get package.json path relative to this file
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const packageJsonPath = join(__dirname, "../package.json");
+const packageJson = JSON.parse(readFileSync(packageJsonPath, "utf-8"));
 
 const program = new Command();
 
@@ -8,7 +17,7 @@ program
   .description(
     "A powerful CLI tool for migrating fullstack web projects between frameworks"
   )
-  .version("1.0.3");
+  .version(packageJson.version);
 
 // Register commands
 program.addCommand(createDetectCommand());
